21e27febd31e85d1c2aaac424bd47bd8ca723377
suryadwipayanaa <kadeksurya60@gmai.com>
2021-05-03 16:04:03 +0800

    project pertama

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>Prapen Mas</title>
+    <link rel="icon" href="koperasi.jpg">
+    <link rel="stylesheet" href="cessss.css">
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js"></script>
+</head>
+<body>
+    <div class="navbar">
+        <div class="map">
+                <li><i class="fa fa-map-marker" aria-hidden="true"></i>
+        </div>
+
+        <div class="icon"> 
+            <img src="koperasi4.jpeg" alt="koperasi">
+        </div>
+        <header align="center">
+            <h1>KOPERASI SIMPAN PINJAM</h1>
+                <h2>PRAPEN MAS</h2>
+                    <p>Jln, Getas Kawan , Buruan , Blahbatuh , Gianyar</p>
+        </header>
+        <div class="menubar">
+            <ul>
+                <li>
+                        <i class="fa fa-home" aria-hidden="true"></i><a href="#home">Home </a>
+                        <i class="fa fa-cogs" aria-hidden="true"></i><a href="#about">About</a>
+                        <i class="far fa-images"></i><a href="#galeri">Galeri</a>
+                        <i class="fas fa-phone-square-alt"></i><a href="#contact">Contact</a>
+                        <i class="fa fa-user" aria-hidden="true"></i><a href="#anggota">Anggota </a>
+                </li>
+            </ul>
+        </div>
+    </div>  
+    <div class="konten" id="home">
+        <div class="gambar">
+            <img src="pp.jpg" alt="pp">
+        </div>
+
+            <p> <b>Pengertian Koperasi Berdasarkan Ahli</b>
+                <br>
+                    <br>
+                Secara umum, koperasi dapat diartikan sebagai badan usaha yang dimiliki serta dikelola para anggotanya. Namun, ada pengertian lain dari koperasi menurut beberapa ahli. Salah satunya dari Bapak Koperasi, Mohammad Hatta. Menurutnya, koperasi adalah usaha bersama guna memperbaiki atau meningkatkan kehidupan atau taraf ekonomi berlandaskan asas tolong menolong.
+
+                    Sementara itu, Arifinal Chaniago mengartikan koperasi sebagai suatu perkumpulan yang bekerja sama dalam menjalankan sebuah usaha secara kekeluargaan guna meningkatkan kesejahteraan anggotanya. Pengelolaan sebuah koperasi, para anggotanya dapat dengan bebas untuk keluar dan masuk dari badan usaha tersebut. 
+                    
+                    Arti koperasi oleh Munkner adalah organisasi berasaskan tolong menolong yang mengelola ‘urusniaga’ secara berkelompok. Tujuannya meningkatkan urusan ekonomi, berbeda dengan asas gotong royong yang bertujuan membangun kebutuhan sosial.
+                    
+                    Berdasarkan Undang-undang (UU) Nomor 25 Tahun 1992 tentang Perkoperasian, pada Pasal 1 dijelaskan, koperasi adalah badan usaha yang beranggotakan orang-seorang atau badan hukum koperasi dengan melandaskan kegiatannya berdasar prinsip koperasi, sekaligus sebagai gerakan ekonomi rakyat atas asas kekeluargaan. Sedangkan perkoperasian adalah segala sesuatu yang menyangkut kehidupan koperasi.
+                    <br>
+                    <br>
+                    <b>Tujuan Di Bangunya Koperasi?</b>
+                    <br>
+
+                    <br>
+                    Berdasarkan pengertian koperasi secara umum dan para ahli, pembentukan koperasi bertujuan untuk meningkatkan kesejahteraan ekonomi dari para anggotanya. Tujuan lainnya, antara lain:
+                    1. Membantu memperbaiki taraf hidup maupun ekonomi para anggotanya serta masyarakat sekitar.
+                    
+                    2. Membantu pemerintah mewujudkan kehidupan masyarakat yang adil dan makmur.
+                    
+                    3. Meningkatkan tatanan perekonomian di Indonesia.
+                </p>
+
+                </div>
+
+                <div class="about" id="about">
+                    <h1>KOPERASI PRAPEN MAS</h1>
+                    <br>
+                    <hr>
+                    <img src="gambar2.jpg" alt="gambar2">
+                    <p>
+                        Koperasi Prapen Mas mulai didirikan pada tanggal 20 februari 2017, koperasi ini didirikan oleh bebrapa orang sebagain anggota dari koperasi ini adalah keluarga yang berkasta pande dengan hal tersebut maka nama koperasinya adalah prapen mas.
+                        Koperasi ini berada di blahbatuh gianyar bali koprasi ini juga sudah pernah mengadakan rapat tahunan atau yang biasa kita dengar adalah RAT,
+                        Selain bank, cukup banyak orang mendapat dana pinjaman dari koperasi simpan pinjam (KSP). Keberadaan KSP dinilai menjadi alternatif kalau gak memperoleh dana pinjaman dari bank.
+
+Selain memperoleh dana pinjaman, orang-orang juga bisa menikmati imbal hasil dengan menyimpan dana yang dimilikinya di KSP. Sama seperti bank, KSP memberi imbal hasil berupa bunga koperasi simpan pinjam yang nantinya diberikan tiap bulan.
+
+Meski KSP mirip-mirip dengan bank, keduanya sebenarnya punya tujuan pendirian yang berbeda. 
+
+KSP berdiri dengan tujuan meningkatkan kesejahteraan para anggotanya. Sementara bank cenderung punya tujuan mencari keuntungan.
+
+Memang sih dengan adanya koperasi, pilihan orang-orang dalam meraih dana pinjaman menjadi beragam. Namun, hadirnya KSP juga perlu diwaspadai. Pasalnya, sebelumnya pernah muncul kasus penipuan berkedok koperasi.
+
+Kasus penipuan berkedok koperasi ini biasanya menggunakan modus iming-iming imbal hasil atau bunga koperasi simpan pinjam tinggi bagi yang mendaftarkan diri sebagai anggota dan menyetorkan dananya. Biasanya oknum koperasi menyebut setoran dana ini sebagai investasi.
+                   
+                    </p>
+                    </div>
+                </div>
+                </div>
+                <div class="content1" id="galeri">
+                <h1 class="cess" align="center">Dokumentasi RAT</h1>
+                <div class="sub1">
+            <ul>
+   
+            <li> 
+              <div class="rat1">
+        <img src="galeri1.jpg">
+            <p class="sub"><span>RAT Tahun 2018</span><br><br>RAT atau singkatan dari Rapat Anggota Tahunan merupakan agenda wajib setiap badan usaha koperasi, karena di dalamnya akan dibahas tentang pertanggunjawaban pengurus koperasi selama satu tahun kepada anggota koperasi yang bersangkutan. Rapat Anggota merupakan pemegang kekuasaan tertinggi didalam pengambilan keputusandi koperasi</p>
+            <a href="#"><button class="submit" type="submit" name="submit">Read More</button></a>
+        </div>
+            </li>
+
+          <li>
+            <div class="rat2">
+            <img src="galeri3.jpg">
+            <p class="sub"> <span>RAT Tahun 2019</span><br><br>RAT atau singkatan dari Rapat Anggota Tahunan merupakan agenda wajib setiap badan usaha koperasi, karena di dalamnya akan dibahas tentang pertanggunjawaban pengurus koperasi selama satu tahun kepada anggota koperasi yang bersangkutan. Rapat Anggota merupakan pemegang kekuasaan tertinggi didalam pengambilan keputusandi koperasi</p> 
+           <a href="#"><button class="submit" type="submit" name="submit">Read More</button></a>
+        </div>
+            </li>
+
+            <li>
+            <div class="rat3">
+            <img src="galeri4.jpg">
+            <p class="sub"><span>RAT Tahun 2020</span><br><br>RAT atau singkatan dari Rapat Anggota Tahunan merupakan agenda wajib setiap badan usaha koperasi, karena di dalamnya akan dibahas tentang pertanggunjawaban pengurus koperasi selama satu tahun kepada anggota koperasi yang bersangkutan. Rapat Anggota merupakan pemegang kekuasaan tertinggi didalam pengambilan keputusandi koperasi</p> 
+           <a href="#"><button class="submit" type="submit" name="submit">Read More</button></a>
+        </div>
+            </li>
+           </ul>
+         </div>  
+         </div>  
+
+               <div class="contact" id="contact">
+                   <h1>Contact Ketua Koperasi</h1>
+                   <br>
+                   <hr>
+        <div class="gambar">
+            <img src="ketua.jpg" alt="ketua">
+        </div>
+        <div class="kontak">
+                <li><i class="fa fa-user" aria-hidden="true"></i><span>I Ketut Patra Asmara</span> </li>
+                <li><i class="fa fa-map-marker" aria-hidden="true"></i><span>Br.Tegallinggah, Bedulu, Gianyar</span></li>
+                <li><i class="fas fa-clock"></i><span>08.00 AM - 13.00 PM</span></li>
+                <li><i class="fa fa-phone" aria-hidden="true"></i><span>081338728925</span></li>
+                <li><i class="fab fa-instagram"></i><span>ketutpatra</span></li>
+                <li><i class="fab fa-facebook"></i><span>ketutpatra</span></li>
+                <li><i class="fab fa-whatsapp"></i><span>083847792188</span></li>
+            </ul>
+             </div>
+                </div>
+        </div>
+        <footer>
+            <p>Copiright By Surya Dwipayana</p>
+        </footer>
+</body>
